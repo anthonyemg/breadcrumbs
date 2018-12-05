0635d999526849f0a52c4499d6451bfdee24d7bd
Anthony Greenheck <aemgreenheck@gmail.com>
2018-12-04 23:05:33 -0500

    Added initial file structure

---
+++ src/public/app/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from  'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        breadcrumbs
+      </div>
+    )
+  }
+}
+
+export default Landing;
---
+++ src/public/app/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Landing } from '../../components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Landing />
+      </div>
+    )
+  }
+}
+
+export default App;
